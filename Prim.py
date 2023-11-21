3cdf28dbc1d2a2c1065541178711a76c92aa1edb
Axel <axel.lopezs@alumno.buap.mx>
2023-11-20 19:50:23 -0600

    five commit

--- Prim.py
+++ Prim.py
@@ -1,55 +1,36 @@
-def prim(graph, root):
-    assert type(graph) == dict
+#Algoritmo de prim
+def prim(w,n,s):
+    v=[]
+    while(len(v) != n):
+        v.append(0)
+    v[s] = 1
+    E = []   
+    suma = 0
+    for i in range(0,n-1):
+        minimo = 9
+        agregar_vertice = 0
+        e = []
+        for j in range(0,n):
+            if(v[j] == 1):
+                for k in range(0,n):
+                    if(v[k]==0 and w[j][k] < minimo):
+                        agregar_vertice = k
+                        e = [j,k]
+                        minimo = w[j][k]
+        suma += w[e[0]][e[1]]
+        v[agregar_vertice] = 1
+        E.append(e)
+    return [E,suma]
 
-    nodes = list(graph.keys())
-    nodes.remove(root)
+n = 6
+s = 0
+w = [#1,2,3,4,5,6  #9 == No existe arista que conecta a esos vertices
+     [9,3,1,2,9,9],#1
+     [3,9,9,9,2,4],#2
+     [1,9,9,5,2,9],#3
+     [2,9,5,9,1,9],#4
+     [9,2,2,1,9,2],#5
+     [9,4,9,9,2,9],#6
+    ]
 
-    visited = [root]
-    path = []
-    next_node = None
-
-    while nodes:
-        distance = float('inf')
-        for s in visited:
-            for d in graph[s]:
-                if d in visited or s == d:
-                    continue
-                if graph[s][d] < distance:
-                    distance = graph[s][d]
-                    pre = s
-                    next_node = d
-        path.append((pre, next_node))
-        visited.append(next_node)
-        nodes.remove(next_node)
-
-    return path
-
-if __name__ == '__main__':
-    graph_dict = {
-        "s1": {"s1": 0, "s2": 2, "s10": 3, "s12": 4, "s5": 3},
-        "s2": {"s1": 1, "s2": 0, "s10": 4, "s12": 2, "s5": 2},
-        "s10": {"s1": 2, "s2": 6, "s10": 0, "s12": 3, "s5": 4},
-        "s12": {"s1": 3, "s2": 5, "s10": 2, "s12": 0, "s5": 2},
-        "s5": {"s1": 3, "s2": 5, "s10": 2, "s12": 4, "s5": 0},
-    }
-
-    while True:
-        print("\n=== Menú ===")
-        print("1. Ejecutar algoritmo de Prim")
-        print("2. Salir")
-
-        choice = input("Seleccione una opción (1/2): ")
-
-        if choice == '1':
-            root_node = input("Ingrese el nodo raíz: ")
-            if root_node in graph_dict:
-                path = prim(graph_dict, root_node)
-                print("Camino mínimo:")
-                print(path)
-            else:
-                print(f"El nodo '{root_node}' no existe en el grafo.")
-        elif choice == '2':
-            print("Saliendo del programa. ¡Hasta luego!")
-            break
-        else:
-            print("Opción no válida. Por favor, seleccione 1 o 2.")
+print(prim(w,n,s))
